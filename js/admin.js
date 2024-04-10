24ed463655f9eab03ee4a6c41ac7d4c9a55b3082
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2024-04-10 18:48:50 +0900

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -18,21 +18,72 @@ main.listen(serverstart_port,function(){
 main.set("views", "./mainpage");
 
 const admintemplatePath = path.join(__dirname, "/../mainpage/adminpagetamplate/adminpagetamplate.html");
-const userdata = "D:/"
 
-main.get("/mainadmin", (req, res) => {
+
+main.get("/", (req, res) => {
     // 탬플릿 안에 넣을 페이지 html파일 경로
-    const pagePath = path.join(__dirname, "/../mainpage/admin/coffeemania_admin.html");
+    const pagePath = path.join(__dirname, "/../mainpage/admin/adminmain.html");
 
     // 페이지 내용을 템플릿에 적용하여 렌더링
     let renderedTemplate = applyPageToTemplate(admintemplatePath, pagePath);
-
-    const jsonData = {
-        name: "John Doe",
-        age: 30
-    };
     // 렌더링된 템플릿을 클라이언트에게 응답
-    res.send(renderedTemplate).json(jsonData);
+    res.send(renderedTemplate);
+});
+main.get("/mainadmin", (req, res) => {
+    // 탬플릿 안에 넣을 페이지 html파일 경로
+    const pagePath = path.join(__dirname, "/../mainpage/admin/adminmain.html");
+
+    // 페이지 내용을 템플릿에 적용하여 렌더링
+    let renderedTemplate = applyPageToTemplate(admintemplatePath, pagePath);
+    // 렌더링된 템플릿을 클라이언트에게 응답
+    res.send(renderedTemplate);
+});
+
+main.get("/marketadmin", (req, res) => {
+    // 탬플릿 안에 넣을 페이지 html파일 경로
+    const pagePath = path.join(__dirname, "/../mainpage/admin/adminmarket.html");
+
+    // 페이지 내용을 템플릿에 적용하여 렌더링
+    let renderedTemplate = applyPageToTemplate(admintemplatePath, pagePath);
+    // 렌더링된 템플릿을 클라이언트에게 응답
+    res.send(renderedTemplate);
+});
+
+main.get("/buisnessadmin", (req, res) => {
+    // 탬플릿 안에 넣을 페이지 html파일 경로
+    const pagePath = path.join(__dirname, "/../mainpage/admin/adminbuisness.html");
+
+    // 페이지 내용을 템플릿에 적용하여 렌더링
+    let renderedTemplate = applyPageToTemplate(admintemplatePath, pagePath);
+    // 렌더링된 템플릿을 클라이언트에게 응답
+    res.send(renderedTemplate);
+});
+main.get("/operationadmin", (req, res) => {
+    // 탬플릿 안에 넣을 페이지 html파일 경로
+    const pagePath = path.join(__dirname, "/../mainpage/admin/adminoperation.html");
+
+    // 페이지 내용을 템플릿에 적용하여 렌더링
+    let renderedTemplate = applyPageToTemplate(admintemplatePath, pagePath);
+    // 렌더링된 템플릿을 클라이언트에게 응답
+    res.send(renderedTemplate);
+});
+main.get("/selleradmin", (req, res) => {
+    // 탬플릿 안에 넣을 페이지 html파일 경로
+    const pagePath = path.join(__dirname, "/../mainpage/admin/adminseller.html");
+
+    // 페이지 내용을 템플릿에 적용하여 렌더링
+    let renderedTemplate = applyPageToTemplate(admintemplatePath, pagePath);
+    // 렌더링된 템플릿을 클라이언트에게 응답
+    res.send(renderedTemplate);
+});
+main.get("/statisticsadmin", (req, res) => {
+    // 탬플릿 안에 넣을 페이지 html파일 경로
+    const pagePath = path.join(__dirname, "/../mainpage/admin/adminstatistics.html");
+
+    // 페이지 내용을 템플릿에 적용하여 렌더링
+    let renderedTemplate = applyPageToTemplate(admintemplatePath, pagePath);
+    // 렌더링된 템플릿을 클라이언트에게 응답
+    res.send(renderedTemplate);
 });
 
 function loadTemplate(templatePath) {
@@ -41,14 +92,10 @@ function loadTemplate(templatePath) {
 function loadPage(pagePath) {
     return fs.readFileSync(pagePath, 'utf-8');
 }
-// 함수: 탬플릿 안에 넣을 페이지 html 적용
 function applyPageToTemplate(templatePath, pagePath) {
     let template = loadTemplate(templatePath);
     let pageContent = loadPage(pagePath);
 
     const mainPageRegex = /<div id="admin_info"><\/div>/;
     return template.replace(mainPageRegex, pageContent);
-}
-function finddata(){
-    
 }
